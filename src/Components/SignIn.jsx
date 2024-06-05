c3d3e71fe75bda78768989eae5b24bbaa06cbd04
sharathkumarviart <sharathkumarvirat@gmail.com>
2024-06-05 16:21:58 +0530

    develp

--- src/Components/SignIn.jsx
+++ src/Components/SignIn.jsx
@@ -35,8 +35,8 @@ export default function SignIn() {
     return (
         <div className='flex items-center justify-center'>
 
-            <div className='flex items-center flex-col gap-3 justify-center max-h-96 max-w-md border-2 rounded-md p-10 bg-slate-500 '>
-                <div className='flex flex-col w-/4 gap-2'>
+            <div className='flex items-center flex-col justify-center gap-3 justify-center h-[24rem] w-[29rem]  p-10 box mt-16 '>
+                <div className='flex flex-col w-[23rem] gap-2'>
                     <label className="input input-bordered flex items-center gap-4">
                         Email
                         <input type="email" className="grow" placeholder="Enter mail" name='email' onChange={handleChange} />
@@ -44,7 +44,7 @@ export default function SignIn() {
                     <label className="input input-bordered flex items-center gap-4">
                         Password
                         <input type={showPass ? "text" : "password"} className="grow" placeholder="enter the password" name='password' onChange={handleChange} />
-                        <svg onClick={() => { setShowPass(!showPass) }} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" /><path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" /></svg>
+                        <svg xmlns="http://www.w3.org/2000/svg" onClick={() => { setShowPass(!showPass) }} viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z" clipRule="evenodd" /></svg>
                     </label>
                     <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-md" onClick={handleSubmit}>Sign In</button>
                 </div>
--- src/Components/SignUp.jsx
+++ src/Components/SignUp.jsx
@@ -44,7 +44,7 @@ export default function SignUp() {
     return (
         <div className='flex items-center justify-center'>
 
-            <div className='flex items-center  flex-col gap-6 justify-center h-[400px] w-[450px] border-2 bg-slate-900 p-1'>
+            <div className='flex items-center  flex-col gap-6 justify-center h-[450px] w-[450px] box mt-12'>
                 <div className='flex flex-col w-[60%] gap-2' >
                     <label className="input input-bordered flex items-center gap-2">
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" /></svg>
